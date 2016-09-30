3b4da8b6bafb79a53cc1b786ffa02fb416885f1a
p32929 <p32929@users.noreply.github.com>
2016-09-30 14:40:22 +0600

    Add files via upload

---
+++ APG(A-Z).c
@@ -0,0 +1,585 @@
+
+#include<stdio.h>
+int f1()
+{
+    int a;
+    for(a=65; a<=90; a++)
+    {
+        printf("%c\n", a);
+    }
+}
+f2()
+{
+    int a,b;
+    for(b=65; b<=90; b++)
+    {
+        for(a=65; a<=90; a++)
+        {
+            printf("%c%c ", b,a);
+        }
+        printf("\n");
+    }
+}
+f3()
+{
+    int a,b,c;
+    for(c=65; c<=90; c++)
+    {
+
+        for(b=65; b<=90; b++)
+        {
+            for(a=65; a<=90; a++)
+            {
+                printf("%c%c%c ", c,b,a);
+            }
+            printf("\n");
+        }
+
+    }
+}
+f4()
+{
+    int a,b,c,d;
+    for(d=65; d<=90; d++)
+    {
+        for(c=65; c<=90; c++)
+        {
+            for(b=65; b<=90; b++)
+            {
+                for(a=65; a<=90; a++)
+                {
+                    printf("%c%c%c%c ", d,c,b,a);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
+f5()
+{
+    int a,b,c,d,e;
+    for(e=65; e<=90; e++)
+    {
+        for(d=65; d<=90; d++)
+        {
+            for(c=65; c<=90; c++)
+            {
+                for(b=65; b<=90; b++)
+                {
+                    for(a=65; a<=90; a++)
+                    {
+                        printf("%c%c%c%c%c ", e,d,c,b,a);
+                    }
+                    printf("\n");
+                }
+            }
+        }
+    }
+}
+f6()
+{
+    int a,b,c,d,e,f;
+    for(f=65; f<=90; f++)
+    {
+        for(e=65; e<=90; e++)
+        {
+            for(d=65; d<=90; d++)
+            {
+                for(c=65; c<=90; c++)
+                {
+
+                    for(b=65; b<=90; b++)
+                    {
+                        for(a=65; a<=90; a++)
+                        {
+                            printf("%c%c%c%c%c%c ", f,e,d,c,b,a);
+                        }
+                        printf("\n");
+                    }
+                }
+            }
+        }
+    }
+}
+f7()
+{
+    int a,b,c,d,e,f,g;
+    for(f=65; f<=90; f++)
+    {
+        for(e=65; e<=90; e++)
+        {
+            for(d=65; d<=90; d++)
+            {
+                for(c=65; c<=90; c++)
+                {
+
+                    for(b=65; b<=90; b++)
+                    {
+                        for(a=65; a<=90; a++)
+                        {
+                            printf("%c%c%c%c%c%c%c ", g,f,e,d,c,b,a);
+                        }
+                        printf("\n");
+                    }
+                }
+            }
+        }
+    }
+}
+f8()
+{
+    int a,b,c,d,e,f,g,h;
+    for(g=65; g<=90; g++)
+    {
+        for(f=65; f<=90; f++)
+        {
+            for(e=65; e<=90; e++)
+            {
+                for(d=65; d<=90; d++)
+                {
+                    for(c=65; c<=90; c++)
+                    {
+
+                        for(b=65; b<=90; b++)
+                        {
+                            for(a=65; a<=90; a++)
+                            {
+                                printf("%c%c%c%c%c%c%c%c ", h,g,f,e,d,c,b,a);
+                            }
+                            printf("\n");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f9()
+{
+    int a,b,c,d,e,f,g,h,i;
+    for(h=65; h<=90; h++)
+    {
+        for(g=65; g<=90; g++)
+        {
+            for(f=65; f<=90; f++)
+            {
+                for(e=65; e<=90; e++)
+                {
+                    for(d=65; d<=90; d++)
+                    {
+                        for(c=65; c<=90; c++)
+                        {
+
+                            for(b=65; b<=90; b++)
+                            {
+                                for(a=65; a<=90; a++)
+                                {
+                                    printf("%c%c%c%c%c%c%c%c%c ", i,h,g,f,e,d,c,b,a);
+                                }
+                                printf("\n");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f10()
+{
+    int a,b,c,d,e,f,g,h,i,j;
+    for(i=65; i<=90; i++)
+    {
+        for(h=65; h<=90; h++)
+        {
+            for(g=65; g<=90; g++)
+            {
+                for(f=65; f<=90; f++)
+                {
+                    for(e=65; e<=90; e++)
+                    {
+                        for(d=65; d<=90; d++)
+                        {
+                            for(c=65; c<=90; c++)
+                            {
+
+                                for(b=65; b<=90; b++)
+                                {
+                                    for(a=65; a<=90; a++)
+                                    {
+                                        printf("%c%c%c%c%c%c%c%c%c%c ", j,i,h,g,f,e,d,c,b,a);
+                                    }
+                                    printf("\n");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f11()
+{
+    int a,b,c,d,e,f,g,h,i,j,k;
+    for(k=65; k<=90; k++)
+    {
+        for(i=65; i<=90; i++)
+        {
+            for(h=65; h<=90; h++)
+            {
+                for(g=65; g<=90; g++)
+                {
+                    for(f=65; f<=90; f++)
+                    {
+                        for(e=65; e<=90; e++)
+                        {
+                            for(d=65; d<=90; d++)
+                            {
+                                for(c=65; c<=90; c++)
+                                {
+
+                                    for(b=65; b<=90; b++)
+                                    {
+                                        for(a=65; a<=90; a++)
+                                        {
+                                            printf("%c%c%c%c%c%c%c%c%c%c%c ", k,j,i,h,g,f,e,d,c,b,a);
+                                        }
+                                        printf("\n");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f12()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l;
+    for(l=65; l<=90; l++)
+    {
+        for(k=65; k<=90; k++)
+        {
+            for(i=65; i<=90; i++)
+            {
+                for(h=65; h<=90; h++)
+                {
+                    for(g=65; g<=90; g++)
+                    {
+                        for(f=65; f<=90; f++)
+                        {
+                            for(e=65; e<=90; e++)
+                            {
+                                for(d=65; d<=90; d++)
+                                {
+                                    for(c=65; c<=90; c++)
+                                    {
+
+                                        for(b=65; b<=90; b++)
+                                        {
+                                            for(a=65; a<=90; a++)
+                                            {
+                                                printf("%c%c%c%c%c%c%c%c%c%c%c%c ", l,k,j,i,h,g,f,e,d,c,b,a);
+                                            }
+                                            printf("\n");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f13()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m;
+    for(m=65; m<=90; m++)
+    {
+        for(l=65; l<=90; l++)
+        {
+            for(k=65; k<=90; k++)
+            {
+                for(i=65; i<=90; i++)
+                {
+                    for(h=65; h<=90; h++)
+                    {
+                        for(g=65; g<=90; g++)
+                        {
+                            for(f=65; f<=90; f++)
+                            {
+                                for(e=65; e<=90; e++)
+                                {
+                                    for(d=65; d<=90; d++)
+                                    {
+                                        for(c=65; c<=90; c++)
+                                        {
+
+                                            for(b=65; b<=90; b++)
+                                            {
+                                                for(a=65; a<=90; a++)
+                                                {
+                                                    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c ", m,l,k,j,i,h,g,f,e,d,c,b,a);
+                                                }
+                                                printf("\n");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f14()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n;
+    for(n=65; n<=90; n++)
+    {
+        for(m=65; m<=90; m++)
+        {
+            for(l=65; l<=90; l++)
+            {
+                for(k=65; k<=90; k++)
+                {
+                    for(i=65; i<=90; i++)
+                    {
+                        for(h=65; h<=90; h++)
+                        {
+                            for(g=65; g<=90; g++)
+                            {
+                                for(f=65; f<=90; f++)
+                                {
+                                    for(e=65; e<=90; e++)
+                                    {
+                                        for(d=65; d<=90; d++)
+                                        {
+                                            for(c=65; c<=90; c++)
+                                            {
+
+                                                for(b=65; b<=90; b++)
+                                                {
+                                                    for(a=65; a<=90; a++)
+                                                    {
+                                                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c ", n,m,l,k,j,i,h,g,f,e,d,c,b,a);
+                                                    }
+                                                    printf("\n");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f15()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o;
+    for(o=65; o<=90; o++)
+    {
+        for(n=65; n<=90; n++)
+        {
+            for(m=65; m<=90; m++)
+            {
+                for(l=65; l<=90; l++)
+                {
+                    for(k=65; k<=90; k++)
+                    {
+                        for(i=65; i<=90; i++)
+                        {
+                            for(h=65; h<=90; h++)
+                            {
+                                for(g=65; g<=90; g++)
+                                {
+                                    for(f=65; f<=90; f++)
+                                    {
+                                        for(e=65; e<=90; e++)
+                                        {
+                                            for(d=65; d<=90; d++)
+                                            {
+                                                for(c=65; c<=90; c++)
+                                                {
+
+                                                    for(b=65; b<=90; b++)
+                                                    {
+                                                        for(a=65; a<=90; a++)
+                                                        {
+                                                            printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c ", o,n,m,l,k,j,i,h,g,f,e,d,c,b,a);
+                                                        }
+                                                        printf("\n");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+f16()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+    for(p=65; p<=90; p++)
+    {
+        for(o=65; o<=90; o++)
+        {
+            for(n=65; n<=90; n++)
+            {
+                for(m=65; m<=90; m++)
+                {
+                    for(l=65; l<=90; l++)
+                    {
+                        for(k=65; k<=90; k++)
+                        {
+                            for(i=65; i<=90; i++)
+                            {
+                                for(h=65; h<=90; h++)
+                                {
+                                    for(g=65; g<=90; g++)
+                                    {
+                                        for(f=65; f<=90; f++)
+                                        {
+                                            for(e=65; e<=90; e++)
+                                            {
+                                                for(d=65; d<=90; d++)
+                                                {
+                                                    for(c=65; c<=90; c++)
+                                                    {
+
+                                                        for(b=65; b<=90; b++)
+                                                        {
+                                                            for(a=65; a<=90; a++)
+                                                            {
+                                                                printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c ", p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a);
+                                                            }
+                                                            printf("\n");
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+main()
+{
+    int n;
+    printf("How many characters?\n");
+    scanf("%d", &n);
+    do
+    {
+        if(n==1)
+        {
+            f1();
+            break;
+        }
+        else if(n==2)
+        {
+            f2();
+            break;
+        }
+        else if(n==3)
+        {
+            f3();
+            break;
+        }
+        else if(n==4)
+        {
+            f4();
+            break;
+        }
+        else if(n==5)
+        {
+            f5();
+            break;
+        }
+        else if(n==6)
+        {
+            f6();
+            break;
+        }
+        else if(n==7)
+        {
+            f7();
+            break;
+        }
+        else if(n==8)
+        {
+            f8();
+            break;
+        }
+        else if(n==9)
+        {
+            f9();
+            break;
+        }
+        else if(n==10)
+        {
+            f10();
+            break;
+        }
+        else if(n==11)
+        {
+            f11();
+            break;
+        }
+        else if(n==12)
+        {
+            f12();
+            break;
+        }
+        else if(n==13)
+        {
+            f13();
+            break;
+        }
+        else if(n==14)
+        {
+            f14();
+            break;
+        }
+        else if(n==15)
+        {
+            f15();
+            break;
+        }
+        else if(n==16)
+        {
+            f16();
+            break;
+        }
+        else if (n==0)
+        {
+            printf("End\n") ;
+            break;
+        }
+    }
+    while (n!=0);
+    return 0;
+}
